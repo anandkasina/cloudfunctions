f4d9e7bbe801ab6f923f53012c48946abaa3175e
kasina <kasina.anand@gmail.com>
2024-02-12 16:34:50 +0530

    testing

--- cloud-function/outputs.tf
+++ cloud-function/outputs.tf
@@ -2,7 +2,7 @@ output "function_metadata" {
   value       = { uri = google_cloudfunctions2_function.function.service_config[0].uri }
   description = "Details of the cloud function deployed"
 }
-
+#updated for testing
 output "function_out" {
   value       = google_cloudfunctions2_function.function.service_config[0]
   description = "Details of the cloud function deployed"
